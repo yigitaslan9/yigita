8404cbe3b493c5c01024bce6b82c57fc72a5477a
Yigit Aslan <142639939+yigitaslan9@users.noreply.github.com>
2023-08-20 20:26:29 +0300

    Add files via upload

---
+++ app/src/main/java/com/exasadmple/yigita/MainActivity.java
@@ -0,0 +1,45 @@
+package com.exasadmple.yigita;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+
+		setContentView:(R.layout.activity_main);
+
+		Button button1=(Button) findViewById(R.id.button1);
+
+		button1.setOnClickListener(new View.OnClickListener() {
+
+
+
+			@Override
+			public void onClick(View v) {
+				Uri linkimiz=Uri.parse("http://www.mynet.com/");
+				Intent intentimiz=new Intent(Intent.ACTION_VIEW, linkimiz);
+				startActivity(intentimiz);
+
+			}
+		});
+
+	}
+;
+			   
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
